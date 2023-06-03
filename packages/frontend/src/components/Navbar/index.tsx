33318f3d5f773a1a0775a0064f7904e6e834ec3d
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-03 19:21:45 -0300

    feat: change <a> elements to <Link> elements

--- packages/frontend/src/components/Navbar/index.tsx
+++ packages/frontend/src/components/Navbar/index.tsx
@@ -4,28 +4,29 @@ import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
 import largeLogo from '../../assets/img/logo-large-transparent.png';
 import smallLogo from '../../assets/img/logo-small.png';
 import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav id={styles.navbar}>
-      <a href="/">
+      <Link to="/">
         <img src={largeLogo} alt="Banga's Logo" id={styles.largeLogo} />
         <img src={smallLogo} alt="Banga's Logo" id={styles.smallLogo} />
-      </a>
+      </Link>
       <ul>
         <li>
-          <a href="/">
+          <Link to="/cart">
             <i aria-label="Cart">
               <FontAwesomeIcon icon={faCartShopping} />
             </i>
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/">
+          <Link to="/profile">
             <i aria-label="Profile">
               <FontAwesomeIcon icon={faUser} />
             </i>
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
