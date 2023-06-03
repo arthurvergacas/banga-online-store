08dbf881d1c2c951ba2947fe82ee120fb6f5d59a
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-03 18:50:08 -0300

    feat: create basic navbar structure

--- packages/frontend/src/components/Navbar/index.tsx
+++ packages/frontend/src/components/Navbar/index.tsx
@@ -1,5 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+
+import largeLogo from '../../assets/img/logo-large-transparent.png';
+import smallLogo from '../../assets/img/logo-small.png';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
-  return <div className={styles.container}>Oi aqui na navbar</div>;
+  return (
+    <nav id={styles.navbar}>
+      <a href="/">
+        <img src={largeLogo} alt="Banga's Logo" id={styles.largeLogo} />
+        <img src={smallLogo} alt="Banga's Logo" id={styles.smallLogo} />
+      </a>
+      <ul>
+        <li>
+          <a href="/">
+            <i aria-label="Cart">
+              <FontAwesomeIcon icon={faCartShopping} />
+            </i>
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <i aria-label="Profile">
+              <FontAwesomeIcon icon={faUser} />
+            </i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
