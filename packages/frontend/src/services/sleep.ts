e1d9f0be13dca3f08c5bd4062a1e35213a1a0312
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-04 14:13:53 -0300

    chore: add todo to delete sleep later

--- packages/frontend/src/services/sleep.ts
+++ packages/frontend/src/services/sleep.ts
@@ -1,3 +1,5 @@
+// TODO delete this when we change to real api calls
+
 export default function sleep(ms: number = 500): Promise<void> {
   return new Promise((res) => {
     setTimeout(() => res(), ms);
