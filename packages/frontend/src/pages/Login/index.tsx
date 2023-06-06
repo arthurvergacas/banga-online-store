7f3b56e8e625f97a5c1291f4e2f6972c09be1e50
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-06 20:33:38 -0300

    feat: craete login page base structure

--- packages/frontend/src/pages/Login/index.tsx
+++ packages/frontend/src/pages/Login/index.tsx
@@ -1,3 +1,25 @@
+import Button from 'components/Button';
+import { Link } from 'react-router-dom';
+
+import styles from './Login.module.css';
+import Input from 'components/Input';
+
 export default function Login() {
-  return <h1>login</h1>;
+  return (
+    <div id={styles.container}>
+      <form id={styles.loginForm}>
+        <h1>Login</h1>
+
+        <Input label="Email" name="email" type="email" placeholder="Email" width="min(80%, 18em)" />
+
+        <Input label="Senha" name="password" type="password" placeholder="Senha" width="min(80%, 18em)" />
+
+        <Button type="submit">ENTRAR</Button>
+      </form>
+
+      <small>Esqueceu sua senha? Contate um administrador.</small>
+
+      <Link to="/sign-up">Não possui uma conta?</Link>
+    </div>
+  );
 }
