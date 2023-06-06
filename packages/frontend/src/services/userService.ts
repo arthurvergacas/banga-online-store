1f650e83c13112865688be3dfe9741cf7b0cf87a
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-06 17:11:42 -0300

    feat: create login methods in user service

--- packages/frontend/src/services/userService.ts
+++ packages/frontend/src/services/userService.ts
@@ -9,14 +9,25 @@ const mockUser: User = {
   phone: '+55 (16) 98765-4321',
   cpf: '123.456.789-0',
   rg: '12.345.678-9',
-  birthDate: new Date('2003-03-25T03:00'),
+  birthDate: new Date('2003-03-25T00:00'),
   address: 'Rua XIX de Abril, 2023',
 };
+
+let userSessionCreated = false;
 
 const UserService = {
   getUserData: async (): Promise<User> => {
     await sleep();
     return mockUser;
+  },
+
+  isUserLoggedIn: (): boolean => {
+    return userSessionCreated;
+  },
+
+  login: async (): Promise<void> => {
+    await sleep();
+    userSessionCreated = true;
   },
 };
 
