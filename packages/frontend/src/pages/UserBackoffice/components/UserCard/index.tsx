3a4cdf3a4f3af83c3d586d7a53bb84b58247e41c
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-07 17:51:49 -0300

    feat: add hover effect on product card

--- packages/frontend/src/pages/UserBackoffice/components/UserCard/index.tsx
+++ packages/frontend/src/pages/UserBackoffice/components/UserCard/index.tsx
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom';
 import styles from './UserCard.module.css';
 
 export default function UserCard() {
   return (
-    <div className={styles.userCard}>
+    <Link to="id" className={styles.userCard}>
       <header>
         <h3>Arthur Januher</h3>
         <h4>arth.janu@usp.br</h4>
       </header>
 
       <span>ADM</span>
-    </div>
+    </Link>
   );
 }
