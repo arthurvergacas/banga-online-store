1c004d0701425e916127bbbe8056db683c770d9c
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-07 10:55:03 -0300

    feat: add onClick props to button

--- packages/frontend/src/components/Button/index.tsx
+++ packages/frontend/src/components/Button/index.tsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import styles from './Button.module.css';
 
 interface ButtonProps {
   type?: 'button' | 'submit' | 'reset' | undefined;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({ children, type }: React.PropsWithChildren<ButtonProps>) {
+export default function Button({ children, type, onClick }: React.PropsWithChildren<ButtonProps>) {
   return (
-    <button type={type ?? 'button'} id={styles.btn}>
+    <button type={type ?? 'button'} id={styles.btn} onClick={onClick}>
       {children}
     </button>
   );
