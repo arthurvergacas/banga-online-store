1fb469c1a539cfaab7700742ffb07799b0d72df9
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-07 11:21:59 -0300

    feat: add disabled prop to button component

--- packages/frontend/src/components/Button/index.tsx
+++ packages/frontend/src/components/Button/index.tsx
@@ -4,11 +4,12 @@ import styles from './Button.module.css';
 interface ButtonProps {
   type?: 'button' | 'submit' | 'reset' | undefined;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
 }
 
-export default function Button({ children, type, onClick }: React.PropsWithChildren<ButtonProps>) {
+export default function Button({ children, type, onClick, disabled }: React.PropsWithChildren<ButtonProps>) {
   return (
-    <button type={type ?? 'button'} id={styles.btn} onClick={onClick}>
+    <button type={type ?? 'button'} id={styles.btn} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
