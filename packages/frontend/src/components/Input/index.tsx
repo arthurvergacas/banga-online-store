09231764879687b2c8b90db5d6a68bd2d7e48448
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-06 21:00:04 -0300

    feat: add react hook form dependency

--- packages/frontend/src/components/Input/index.tsx
+++ packages/frontend/src/components/Input/index.tsx
@@ -1,9 +1,12 @@
+import { UseFormRegister } from 'react-hook-form';
 import styles from './Input.module.css';
 
 interface InputProps {
   name: string;
+  register: UseFormRegister<any>;
   label?: string;
   placeholder?: string;
+  required?: boolean;
   type?: React.HTMLInputTypeAttribute;
   width?: string;
 }
@@ -13,7 +16,12 @@ export default function Input(props: InputProps) {
     <label htmlFor={props.name} id={styles.label} style={{ width: props.width }}>
       {props.label}
       <div id={styles.inputContainer}>
-        <input type={props.type ?? 'text'} id={props.name} placeholder={props.placeholder} />
+        <input
+          type={props.type ?? 'text'}
+          id={props.name}
+          placeholder={props.placeholder}
+          {...props.register(props.name, { required: props.required })}
+        />
       </div>
     </label>
   );
--- packages/frontend/src/pages/Login/index.tsx
+++ packages/frontend/src/pages/Login/index.tsx
@@ -1,18 +1,48 @@
 import Button from 'components/Button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import styles from './Login.module.css';
 import Input from 'components/Input';
+import UserService from 'services/userService';
+import { useForm } from 'react-hook-form';
+
+interface LoginInputValues {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
+  const navigate = useNavigate();
+
+  const { register, handleSubmit } = useForm<LoginInputValues>();
+
+  const onSubmit = async (data: LoginInputValues) => {
+    try {
+      await UserService.login(data.email, data.password);
+    } catch {
+      // TODO mostrar umas mensagens de que não conseguiu fazer login
+      console.log('não consegui fazer login');
+      return;
+    }
+
+    navigate('/profile');
+  };
+
   return (
     <div id={styles.container}>
-      <form id={styles.loginForm}>
+      <form id={styles.loginForm} onSubmit={handleSubmit(onSubmit)}>
         <h1>Login</h1>
 
-        <Input label="Email" name="email" type="email" placeholder="Email" width="min(80%, 18em)" />
+        <Input label="Email" register={register} name="email" type="email" placeholder="Email" width="min(80%, 18em)" />
 
-        <Input label="Senha" name="password" type="password" placeholder="Senha" width="min(80%, 18em)" />
+        <Input
+          label="Senha"
+          register={register}
+          name="password"
+          type="password"
+          placeholder="Senha"
+          width="min(80%, 18em)"
+        />
 
         <Button type="submit">ENTRAR</Button>
       </form>
