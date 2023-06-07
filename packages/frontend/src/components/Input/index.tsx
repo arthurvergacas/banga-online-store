d4fe51ebeaa85425ba73878f191138be09506edd
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-06 21:35:14 -0300

    feat: add error handling to input

--- packages/frontend/src/components/Input/index.tsx
+++ packages/frontend/src/components/Input/index.tsx
@@ -6,23 +6,35 @@ interface InputProps {
   register: UseFormRegister<any>;
   label?: string;
   placeholder?: string;
-  required?: boolean;
   type?: React.HTMLInputTypeAttribute;
   width?: string;
+
+  required?: boolean;
+  hasError?: boolean;
 }
 
 export default function Input(props: InputProps) {
+  const errorBorderColorCss = `
+    .${styles.label}[for='${props.name}'] .${styles.inputContainer}:focus-within::after {
+      border-bottom-color: var(--redish);
+    }
+  `;
+
   return (
-    <label htmlFor={props.name} id={styles.label} style={{ width: props.width }}>
-      {props.label}
-      <div id={styles.inputContainer}>
-        <input
-          type={props.type ?? 'text'}
-          id={props.name}
-          placeholder={props.placeholder}
-          {...props.register(props.name, { required: props.required })}
-        />
-      </div>
-    </label>
+    <>
+      {props.hasError && <style>{errorBorderColorCss}</style>}
+
+      <label htmlFor={props.name} className={styles.label} style={{ width: props.width }}>
+        {props.label}
+        <div className={styles.inputContainer}>
+          <input
+            type={props.type ?? 'text'}
+            id={props.name}
+            placeholder={props.placeholder}
+            {...props.register(props.name, { required: props.required })}
+          />
+        </div>
+      </label>
+    </>
   );
 }
