792adfe07e26554e3b60e76d4422f9888c4ba852
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-19 20:44:53 -0300

    feat: add remove from cart button

--- packages/frontend/src/pages/Cart/index.tsx
+++ packages/frontend/src/pages/Cart/index.tsx
@@ -2,14 +2,15 @@ import { useEffect, useState } from 'react';
 import styles from './Cart.module.css';
 import { Product } from '@banga/types/product';
 import Spinner from 'components/Spinner';
-import ProductCard from 'components/ProductCard';
 import CartService from 'services/cartService';
 import Button from 'components/Button';
 import { useNavigate } from 'react-router-dom';
+import CartProductCard from './components/CartProductCard';
 
 export default function Cart() {
   const [products, setProducts] = useState<Product[]>();
   const [productsLoading, setProductsLoading] = useState(true);
+  const [shouldUpdate, setShouldUpdate] = useState(true);
 
   const navigate = useNavigate();
 
@@ -27,6 +28,16 @@ export default function Cart() {
     getProducts();
   }, []);
 
+  useEffect(() => {
+    const getProductsWithoutLoading = async () => {
+      setProducts(await CartService.getAll());
+
+      setShouldUpdate(false);
+    };
+
+    if (shouldUpdate) getProductsWithoutLoading();
+  }, [shouldUpdate]);
+
   return (
     <div className={styles.container}>
       <h1>Carrinho</h1>
@@ -35,7 +46,9 @@ export default function Cart() {
         {productsLoading ? (
           <Spinner width="30%" height="30%" />
         ) : (
-          products?.map((product) => <ProductCard product={product} key={product.id} />)
+          products?.map((product) => (
+            <CartProductCard product={product} key={product.id} onRemove={() => setShouldUpdate(true)} />
+          ))
         )}
       </div>
 
--- packages/frontend/src/services/cartService.ts
+++ packages/frontend/src/services/cartService.ts
@@ -13,6 +13,13 @@ const CartService = {
     const id = Math.floor(Math.random() * 1000).toString();
     mockCartProducts.push({ id, ...product });
   },
+
+  removeFromCart: async (id: Product['id']): Promise<void> => {
+    await sleep();
+
+    const index = mockCartProducts.findIndex((p) => p.id === id);
+    mockCartProducts.splice(index, 1);
+  },
 };
 
 export default CartService;
