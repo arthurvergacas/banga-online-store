dcab5a44926f1a9bcf760798414b7b5121491362
Arthur Vergaças <arthurvdm@gmail.com>
2023-07-08 14:53:52 -0300

    feat: create payment interface

--- packages/types/payment.ts
+++ packages/types/payment.ts
@@ -1,6 +1,13 @@
+import { ProductCart } from './product';
+
 export interface PaymentInfo {
   cardNumber: string;
   cardholder: string;
   expirationDate: Date;
   cvv: number;
+}
+
+export interface Payment {
+  paymentInfo: PaymentInfo;
+  products: ProductCart[];
 }
