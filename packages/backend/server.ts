da11fb4169def3522babdb03f32bb1eac195c176
Arthur Vergaças <arthurvdm@gmail.com>
2023-07-09 10:18:23 -0300

    refactor: rename credentials-manager to conform to naming style

--- packages/backend/server.ts
+++ packages/backend/server.ts
@@ -8,16 +8,23 @@ import Payment from './models/payment';
 import Login from './models/login';
 import Cart from './models/cart';
 
-import CredentialsManager from './services/credentials-manager';
+import CredentialsManager from './services/credentialsManager';
 import { randomUUID } from 'crypto';
 
 const app = express();
 app.use(express.json());
 
 // Connecting to MongoDB
-mongoose.connect('mongodb+srv://beni:asddsa123@banga-store.ezeunly.mongodb.net/banga-store?retryWrites=true&w=majority')
-  .then(() => { console.log('Connected to MongoDB') })
-  .catch((error) => { console.error('Error connecting to MongoDB:', error) });
+mongoose
+  .connect(
+    'mongodb+srv://beni:asddsa123@banga-store.ezeunly.mongodb.net/banga-store?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 // Products ------
 
@@ -26,17 +33,21 @@ app.get('/products', async (req, res) => {
   try {
     const products = await Product.find();
     res.json(products);
-  } catch (error) { res.status(500).json({error: 'Error fetching products'}); }
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching products' });
+  }
 });
 
 // Fetch product by product ID
 app.get('/products/:id', async (req, res) => {
   try {
     const productID = req.params.id;
-    const product = await Product.findOne({'id': productID});
+    const product = await Product.findOne({ id: productID });
     if (product) res.json(product);
-    else res.status(404).json({error: 'Product not found'});
-  } catch (error) { res.status(500).json({error: 'Error fetching product'}); }
+    else res.status(404).json({ error: 'Product not found' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching product' });
+  }
 });
 
 // Register new product
@@ -46,9 +57,11 @@ app.post('/products', async (req, res) => {
     productPayload.id = randomUUID();
     const product = new Product(productPayload);
     const savedProduct = await product.save();
-    console.log("saved the thing!!!!");
+    console.log('saved the thing!!!!');
     res.status(201).json(savedProduct);
-  } catch (error) { res.status(500).json({error: 'Error creating product'}); }
+  } catch (error) {
+    res.status(500).json({ error: 'Error creating product' });
+  }
 });
 
 // Edit product by product ID
@@ -57,20 +70,24 @@ app.put('/products/:id', async (req, res) => {
     const productID = req.params.id;
     const updatePayload = req.body;
     delete updatePayload['id'];
-    const product = await Product.findOneAndUpdate({'id': productID}, updatePayload, {new: true});
+    const product = await Product.findOneAndUpdate({ id: productID }, updatePayload, { new: true });
     if (product) res.json(product);
-    else res.status(404).json({error: 'Product not found'});
-  } catch (error) { res.status(500).json({error: 'Error updating product'}); }
+    else res.status(404).json({ error: 'Product not found' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error updating product' });
+  }
 });
 
 // Delete product by product ID
 app.delete('/products/:id', async (req, res) => {
   try {
     const productID = req.params.id;
-    const product = await Product.findOneAndDelete({'id': productID});
+    const product = await Product.findOneAndDelete({ id: productID });
     if (product) res.sendStatus(204);
-    else res.status(404).json({error: 'Product not found'});
-  } catch (error) { res.status(500).json({error: 'Error deleting product'}); }
+    else res.status(404).json({ error: 'Product not found' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error deleting product' });
+  }
 });
 
 // User Profile ------
@@ -80,17 +97,21 @@ app.get('/users', async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
-  } catch (error) { res.status(500).json({error: 'Error fetching users'}); }
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching users' });
+  }
 });
 
 // Fetch user profile by user ID
 app.get('/users/:id', async (req, res) => {
   try {
     const userID = req.params.id;
-    const user = await User.findOne({'id': userID});
+    const user = await User.findOne({ id: userID });
     if (user) res.json(user);
-    else res.status(404).json({error: 'User not found'});
-  } catch (error) { res.status(500).json({error: 'Error fetching user'}); }
+    else res.status(404).json({ error: 'User not found' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching user' });
+  }
 });
 
 // Create new user
@@ -101,7 +122,9 @@ app.post('/users', async (req, res) => {
     const user = new User(userPayload);
     const savedUser = await user.save();
     res.status(201).json(savedUser);
-  } catch (error) { res.status(500).json({error: 'Error creating user'}); }
+  } catch (error) {
+    res.status(500).json({ error: 'Error creating user' });
+  }
 });
 
 // Edit user's profile by user ID
@@ -110,20 +133,24 @@ app.put('/users/:id', async (req, res) => {
     const userID = req.params.id;
     const updatePayload = req.body;
     delete updatePayload['id'];
-    const user = await User.findOneAndUpdate({'id': userID}, updatePayload, {new: true});
+    const user = await User.findOneAndUpdate({ id: userID }, updatePayload, { new: true });
     if (user) res.json(user);
-    else res.status(404).json({error: 'User not found'});
-  } catch (error) { res.status(500).json({error: 'Error updating user'}); }
+    else res.status(404).json({ error: 'User not found' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error updating user' });
+  }
 });
 
 // Delete a user by user ID
 app.delete('/users/:id', async (req, res) => {
   try {
     const userID = req.params.id;
-    const user = await User.findOneAndDelete({'id': userID});
+    const user = await User.findOneAndDelete({ id: userID });
     if (user) res.sendStatus(204);
-    else res.status(404).json({error: 'User not found'});
-  } catch (error) { res.status(500).json({error: 'Error deleting user'}); }
+    else res.status(404).json({ error: 'User not found' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error deleting user' });
+  }
 });
 
 // Payment ------
@@ -136,8 +163,9 @@ app.post('/payments/', async (req, res) => {
     const payment = new Payment(paymentDetailsPayload);
     const savedPayment = await payment.save();
     res.status(201).json(savedPayment);
-  } catch (error) { res.status(500).json({error: 'Error creating payment data',
-                                          "msg": error}); }
+  } catch (error) {
+    res.status(500).json({ error: 'Error creating payment data', msg: error });
+  }
 });
 
 // User Credentials ------
@@ -151,49 +179,51 @@ app.post('/signin', async (req, res) => {
     const user = new User(userPayload);
     const savedUser = await user.save();
 
-    const credentials = new Login(
-      {'email': savedUser.email,
-       'password': savedUser.password,
-       'userID': savedUser.id}
-    );
+    const credentials = new Login({
+      email: savedUser.email,
+      password: savedUser.password,
+      userID: savedUser.id,
+    });
 
     const savedLogin = await credentials.save();
     const token = jwt.sign(
       { userId: savedLogin.id, email: savedLogin.email },
-      "meudeuseuprecisodeferias"
+      'meudeuseuprecisodeferias'
     );
 
-    res
-    .status(200)
-    .json({
+    res.status(200).json({
       success: true,
-      data: {token: token},
+      data: { token: token },
     });
-  } catch (error) { res.status(500).json({error: 'Error creating account', 'msg': error}); }
+  } catch (error) {
+    res.status(500).json({ error: 'Error creating account', msg: error });
+  }
 });
 
 // Login
 app.post('/login', async (req, res) => {
   try {
-    const credentials = await Login.findOne({'email': req.body.email});
-    if (credentials == null) return res.status(401).json({error: 'Unexisting user.'});
+    const credentials = await Login.findOne({ email: req.body.email });
+    if (credentials == null) return res.status(401).json({ error: 'Unexisting user.' });
 
     const encryptedPassword = CredentialsManager.encryptPassword(req.body.password);
     if (credentials.password !== encryptedPassword)
-      res.status(401).json({error: 'Invalid credentials. Rectify provided data and try again.'});
+      res.status(401).json({ error: 'Invalid credentials. Rectify provided data and try again.' });
 
     const token = jwt.sign(
       { userId: credentials.id, email: credentials.email },
-      "meudeuseuprecisodeferias"
+      'meudeuseuprecisodeferias'
     );
 
-    res
-    .status(200)
-    .json({
+    res.status(200).json({
       success: true,
-      data: {token: token},
+      data: { token: token },
     });
-  } catch (error) { res.status(500).json({error: 'Error loging in', 'msg': error}); }
+  } catch (error) {
+    res.status(500).json({ error: 'Error loging in', msg: error });
+  }
 });
 
-app.listen(3000, () => {console.log('Server started on port 3000');});
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
