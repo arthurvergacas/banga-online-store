85e8efe604a6431f2213d51222270590d2235608
Benício Januário <benicio.januario@nubank.com.br>
2023-07-09 11:45:54 -0300

    ACRODEI ATRASADAO PRARRA BATUCAT

--- packages/backend/models/payment.ts
+++ packages/backend/models/payment.ts
@@ -6,6 +6,12 @@ const PaymentSchema: Schema = new Schema({
   cardholder: { type: String, required: true },
   expirationDate: { type: Date, required: true },
   cvv: { type: Number, required: true },
+  products: [
+    {
+      id: { type: String, required: true },
+      quantity: { type: Number, required: true },
+    }
+  ]
 });
 
 export default mongoose.model('payment-details', PaymentSchema, 'payment-details');
